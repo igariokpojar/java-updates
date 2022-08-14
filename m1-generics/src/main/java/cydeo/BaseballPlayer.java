4667db3ce9a41bef6b664b257e44a3c687cb2978
Igariok <igariokpojar@yahoo.com>
2022-08-14 16:10:04 -0700

    Create Abstract class Player and Extends to Subclasses

---
+++ m1-generics/src/main/java/cydeo/BaseballPlayer.java
@@ -0,0 +1,9 @@
+package cydeo;
+
+public class BaseballPlayer extends Player{
+
+
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ m1-generics/src/main/java/cydeo/FootballPlayer.java
@@ -0,0 +1,8 @@
+package cydeo;
+
+public class FootballPlayer extends Player{
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
