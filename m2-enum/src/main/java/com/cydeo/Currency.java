46173317c44ff19484aa402566ec5ce2e248d423
Igariok <igariokpojar@yahoo.com>
2022-08-14 17:44:05 -0700

    Create  Enum Currency

---
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+public enum Currency { // Enum is a special type of class that holding Constants Objects
+
+
+    // Enum are the Objects of Currency class
+    PENNY,NICKLE,DIME,QUARTER
+
+
+
+}
