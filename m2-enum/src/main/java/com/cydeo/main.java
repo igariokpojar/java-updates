ca1d9bb7462dcae26496ec878dc26177fba17bf6
Igariok <igariokpojar@yahoo.com>
2022-08-14 18:08:09 -0700

    Implement Method

--- m2-enum/src/main/java/com/cydeo/main.java
+++ m2-enum/src/main/java/com/cydeo/main.java
@@ -16,5 +16,41 @@ public class main {
         for (Currency currency : currencies){
             System.out.println(currency);
         }
+        System.out.println("How to use switch case i Enums?");
+
+        Currency myCurrency = Currency.QUARTER;
+
+        switch (myCurrency){
+            case PENNY:
+                System.out.println("It is 1 cent");
+                break;
+            case NICKLE:
+                System.out.println("It is 5 cent");
+                break;
+            case DIME:
+                System.out.println("It is 10 cent");
+                break;
+            case QUARTER:
+                System.out.println("It is 25 cent");
+                break;
+        }
+    }
+
+    private static void calculate(double x,double y,Operation operation){
+
+        switch (operation){
+            case PlUS:
+                System.out.println(x+y);
+                break;
+            case MINUS:
+                System.out.println(x-y);
+                break;
+            case MULTIPLY:
+                System.out.println(x*y);
+                break;
+            case DIVIDE:
+                System.out.println(x/y);
+                break;
+        }
     }
 }
