90466e4b7833fef2ec68704d88da18dbd56ff24b
Igariok <igariokpojar@yahoo.com>
2022-08-14 17:43:30 -0700

    Create  a class CurrencyDemo

---
+++ m2-enum/src/main/java/com/cydeo/CurrencyDemo.java
@@ -0,0 +1,10 @@
+package com.cydeo;
+
+public class CurrencyDemo {
+
+    // Created an  ConstantObject from CurrencyDemo Class
+    public static final CurrencyDemo PENNY = new CurrencyDemo();
+    public static final CurrencyDemo Nickle = new CurrencyDemo();
+    public static final CurrencyDemo DIME = new CurrencyDemo();
+    public static final CurrencyDemo QUARTER = new CurrencyDemo();
+}
