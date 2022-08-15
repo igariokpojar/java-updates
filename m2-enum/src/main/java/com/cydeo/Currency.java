9f8850532ab2096eed8d2fe5f0d767c5639096e6
Igariok <igariokpojar@yahoo.com>
2022-08-14 18:28:35 -0700

    Enum

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -5,8 +5,17 @@ public enum Currency { // Enum is a special type of class that holding Constants
 
 
     // Enum are the Constant Objects of Currency class
-    PENNY,NICKLE,DIME,QUARTER
+    PENNY(1),NICKLE(5),DIME(10),QUARTER(25);
 
 
 
+     private int value;
+
+    Currency(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
--- m2-enum/src/main/java/com/cydeo/Operation.java
+++ m2-enum/src/main/java/com/cydeo/Operation.java
@@ -1,6 +1,11 @@
 package com.cydeo;
 
 public enum Operation {
+    // each Constant is Objects(new) of Operation Class
+    PlUS,MINUS,MULTIPLY,DIVIDE;
 
-    PlUS,MINUS,MULTIPLY,DIVIDE
+    Operation() { // this constructor can not be public... must be private
+
+        System.out.println("Constructor");
+    }
 }
