058e240fdf5a0a1944f6926ffd17a5d84f5aa7c2
Igariok <igariokpojar@yahoo.com>
2022-08-15 11:40:31 -0700

    Lambda

--- m3-lambda/src/main/java/com/cydeo/AppleTest.java
+++ m3-lambda/src/main/java/com/cydeo/AppleTest.java
@@ -12,9 +12,14 @@ public class AppleTest {
         inventory.add(new Apple(100,Color.RED));
         inventory.add(new Apple(400,Color.GREEN));
         inventory.add(new Apple(350,Color.RED));
+
+        List<Apple> heavyApple = filterApples(inventory,new AppleHeavyPredicate());
+        System.out.println(heavyApple);
+        List<Apple> greenApple = filterApples(inventory,new AppleHeavyPredicate());
+        System.out.println(greenApple);
     }
 
-    private static List<Apple> filterApples(List<Apple> inventory,ApplePredicates applePredicates){
+    private static List<Apple> filterApples(List<Apple> inventory,ApplePredicates applePredicates){ // get al the apple base on the action give the result
         List<Apple> result = new ArrayList<>();
 
         for (Apple apple : inventory){
