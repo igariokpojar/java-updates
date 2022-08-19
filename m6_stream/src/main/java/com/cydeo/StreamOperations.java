6530def5b1614339f9e635f5ab1df1ec380ce6e5
Igariok <igariokpojar@yahoo.com>
2022-08-19 11:30:40 -0500

    Stream Operation

--- m6_stream/src/main/java/com/cydeo/StreamOperations.java
+++ m6_stream/src/main/java/com/cydeo/StreamOperations.java
@@ -13,22 +13,42 @@ public class StreamOperations {
         list.forEach(System.out::println); // or use above
 
         System.out.println("---------Filter--------------------------------");
+        //Filtering with a predicate : The stream interface supports a filter method.
+        //This operation takes as argument a predicate (a function returning a boolean) and
+        //returns a stream including all elements that matches the predicate.
 
-        //Filter
         list.stream().filter(i -> i%2==0).forEach(System.out::println);
 
         System.out.println("----------Distinct------------------------------");
+        //Filtering unique elements : The stream interface supports a distinct method. This
+        //operation returns a stream with unique elements.
         // Distinct
         Stream<Integer> str = list.stream().filter(i -> i%2==0).distinct();
         str.forEach(System.out::println); // Steam is close here
 
         System.out.println("---------------Limit----------------------------------");
+        //The stream interface supports a limit(n) method. This operation returns
+        //a stream that’s no longer than a given size.
 
         list.stream().filter(i -> i%2==0).limit(1).forEach(System.out::println);
 
         System.out.println("----------------Skip---------------------------");
+        //The stream interface supports a skip(n) method. This operation returns
+        //a stream that discards the first n element.
+        //If the stream has fewer than n elements, an empty stream is returned.
 
         list.stream().filter(i -> i%2==0).skip(1).forEach(System.out::println);
+
+        System.out.println("----------------Map---------------");
+        //The stream interface supports a map method. This operation takes a
+        //function as argument. The function is applied to each element,
+        //mapping it to a new element.
+
+        list.stream().map(number ->number*2).filter(i ->i%3==0).forEach(System.out::println);
+
+
+
+
 
 
     }
