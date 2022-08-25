7bf7abe88db7eafba22908ab757c42805fcf90ec
Igariok <igariokpojar@yahoo.com>
2022-08-25 11:13:05 -0500

    Updates

--- m5_comparator_interface/src/main/java/com/cydeo/MyComparator.java
+++ m5_comparator_interface/src/main/java/com/cydeo/MyComparator.java
@@ -6,6 +6,6 @@ import java.util.function.Consumer;
 public class MyComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
-        return (o1>o2)? -1 : (o1<o2) ? 1:0;
+        return (o1>o2)? -1 : (o1<o2) ? 1:0; // if o1>02 then return -1; if o1<o2 then return 1; if not  then is equal;
     }
 }
